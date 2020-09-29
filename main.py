70a558ed60f8db8de9f850f27bfe561a4794d648
Chelsea Houston <chelseahouston29@gmail.com>
2020-09-29 16:09:35 +0000

    Added £

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 bike = 2000
 print("Motorbike Value")
 while bike > 1000:
-  print(bike)
+  print(f"£ {bike}")
   tenpercent = bike / 10
   bike = bike - tenpercent
 else:
-    print("Value below 1000")
+    print("Value below £1000")
     
